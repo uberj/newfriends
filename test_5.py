12c89a98d56ffece94dd56d32262f4c2385f0469
Jacques Uber <uberj@miradorfin.com>
2019-11-10 11:49:51 -0800

    [master] sure

---
+++ test_5.py
@@ -0,0 +1,54 @@
+import unittest
+import sys
+
+from pprint import pprint
+from util import *
+import math
+from bitstring import BitArray as BA
+
+def str_to_ba(i:str) -> BA:
+	h = "0x"
+	for c in i:
+		h += hex(ord(c))
+	return BA(h)
+
+def xor_cycle_encrypt(key:BA, m:BA) -> BA:
+	e = key * math.ceil(float(len(m))/len(key))
+	e = e[:len(m)]
+
+	assert len(e) == len(m)
+
+	return e ^ m
+
+
+def ba_to_hex(encrypt:BA) -> str:
+	return encrypt.hex
+
+
+def xor_encrypt(key_text, ms) -> str:
+	key = str_to_ba(key_text)
+	es = []
+	encrypt = xor_cycle_encrypt(key, str_to_ba(ms))
+	es.append(ba_to_hex(encrypt))
+	return "\n".join(es)
+
+
+class Test64(unittest.TestCase):
+	def test_example(self):
+		x = "Burning 'em, if you ain't quick and nimble\r\nI go crazy when I hear a cymbal"
+		# x = "Burning 'em, if you ain't quick and nimble"
+		# x = "I go crazy when I hear a cymbal"
+		e = xor_encrypt("ICE", x)
+		print(e)
+		a_ = "0b3637272a2b2e63622c2e69692a23693a2a3c6324202d623d63343c2a26226324272765272a282b2f20430a652e2c652a3124333a653e2b2027630c692b20283165286326302e27282f"
+		print(a_)
+		self.assertEqual(e, a_)
+		# expected = "0b3637272a2b2e63622c2e69692a23693a2a3c6324202d623d63343c2a26226324272765272a282b2f20430a652e2c652a3124333a653e2b2027630c692b20283165286326302e27282f"
+		# print(expected)
+		# self.assertEqual(e, expected)
+
+if __name__ == "__main__":
+	key = sys.argv[1]
+	fn = sys.argv[2]
+	with open(fn, "r") as fd:
+		print(xor_encrypt(key, fd.read()))
