77f2744e89aa10b369417e582a5685dc6707239e
Jacques Uber <uberj@miradorfin.com>
2019-11-25 21:04:25 -0800

    [master] attack plan recorded

--- test_16.py
+++ test_16.py
@@ -29,3 +29,50 @@ class TestChallenge16(unittest.TestCase):
 		cipher_text = encrypt_data(b"XXXXXX")
 		admin_check = check_admin(cipher_text)
 		self.assertFalse(admin_check)
+
+	def test_attack(self):
+		"""
+		Here are the blocks!
+
+		comment1=cooking
+		%20MCs;userdata=
+		(User here)
+		;comment2=%20lik
+		e%20a%20pound%20
+		of%20bacon
+
+		I want something like:
+
+		%20MCs;userdata=
+		YYYYYYYYYYYYYYYY
+		;admin=true;XXXX
+		;comment2=%20lik
+
+		ord(":") -> 58
+		bin(58) -> 0b111010
+		--- change 1 bit ---
+		0b111010 ^ 0b000001 -> 0b111011
+		0b111011 -> 59
+		chr(59) -> ";"
+
+		Simplified and expanded to the other character:
+		chr(ord(':') ^ 0b000001) -> ';'
+		chr(ord('=') ^ 0b000001) -> '<'
+
+		Following rules I can get:
+
+		0123456789012345
+		YYYYYYYYYYYYYYYY
+		:admin>true:XXXX
+
+		I encrypt...
+		I take my encrypted "YY.." block...
+		I change the 0th, 6th, and 11th  byte to "0b000001"
+
+		I check admin on that.
+		Let us see if that works!
+		(Will try later)
+
+		:return:
+		"""
+		pass
