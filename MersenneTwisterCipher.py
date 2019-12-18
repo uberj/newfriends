34bff64a2df5b9c25bb7314d4796b58edc0095ea
Jacques Uber <uberj@miradorfin.com>
2019-12-17 16:17:29 -0800

    last challenge in set 3

---
+++ MersenneTwisterCipher.py
@@ -0,0 +1,31 @@
+from MersenneTwisterPRNG import MersenneTwisterPRNG
+from somecode import pad16_PKCS7, xor
+from Crypto.Cipher import AES
+
+
+class MersenneTwisterCipher(object):
+	def __init__(self, key: int):
+		self.key = key
+		self.block_size = 16
+		self.half_block_size = int(self.block_size / 2)
+
+	def _mt_next(self, plaintext: bytes) -> bytes:
+		prng = MersenneTwisterPRNG(self.key)
+		plaintext_blocks = [plaintext[i:i + self.block_size] for i in range(0, len(plaintext), self.block_size)]
+		for block in plaintext_blocks:
+			mixin_block = (
+				int.to_bytes(prng.next(), 4, 'big') +
+				int.to_bytes(prng.next(), 4, 'big') +
+				int.to_bytes(prng.next(), 4, 'big') +
+				int.to_bytes(prng.next(), 4, 'big')
+			)
+			for p_byte, e_byte in zip(block, mixin_block):
+				yield p_byte ^ e_byte
+
+	def encrypt(self, plaintext: bytes) -> bytes:
+		return bytes(self._mt_next(plaintext))
+
+	def decrypt(self, ciphertext: bytes) -> bytes:
+		return bytes(self._mt_next(ciphertext))
+
+
---
+++ test_24.py
@@ -0,0 +1,35 @@
+import unittest
+import random
+
+from MersenneTwisterCipher import MersenneTwisterCipher
+from somecode import rand_n_string
+
+R = random.Random()
+rint = lambda: R.randint(0, 2 ** 32 - 1)
+N = 624
+
+
+class TestChallenge24(unittest.TestCase):
+	def test_mt_cipher_round_trip(self):
+		cipher = MersenneTwisterCipher(rint())
+		s = rand_n_string(10000).encode()
+		e = cipher.encrypt(s)
+		d = cipher.decrypt(e)
+		self.assertEqual(s, d)
+
+	def test_infer_secret(self):
+		secret = R.randint(0, 2**8)  # using an 8 bit seed to make the exercise more time friendly
+		cipher = MersenneTwisterCipher(secret)
+		s = rand_n_string(R.randint(10, 40)).encode() + b"A" * 14
+		e = cipher.encrypt(s)
+		attack_string = b"A" * len(s)
+		for i in range(2**32):
+			cipher = MersenneTwisterCipher(i)
+			e_attack = cipher.encrypt(attack_string)
+			if e_attack.endswith(e[-14:]):
+				self.assertEqual(secret, i)
+				return
+
+		self.fail("Didn't find the secret")
+
+
